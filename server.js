30e631d640f2a4a35b1aa35bbb30d2081e774f16
liambarness <liam.barnes@temple.edu>
2025-03-07 15:55:41 -0500

    changed to async and completed app. takes in phonenumbers.json and sends texts to all listed phone numbers

--- server.js
+++ server.js
@@ -1,29 +1,75 @@
 const express = require('express')
-const fs = require('fs');
-const app = express()
-const port = 3000
+const fs = require('fs/promises');
+const twilio = require('twilio');
+require('dotenv').config();
 
-app.get('/', (req, res) => {
-    fs.readFile('phonenumbers.json', (err, json) => {
-        if(err) {
-            console.log(err);
-            return res.status(500).send('Error reading phone numbers');
+const app = express();
+const port = 3000;
 
-        }
+const accountSid = process.env.TWILIO_SID;
+const authToken = process.env.AUTH_TOKEN;
+const twilioNumber = process.env.TWILIO_NUMBER;
+
+const client = twilio(accountSid, authToken);
+
+const msg = 'MSG HERE';
+
+app.get('/', async (req, res) => {
+    try {
+        const json = await fs.readFile('phonenumbers.json');
+        const data = JSON.parse(json);
+
+        const messages = data.map(entry => {
+            const phone = entry.phone;
+            const name = entry.name;
     
 
-    try {
-        const data = JSON.parse(json);
-        console.log(data);
-        res.send(data);
+        if (!phone) {
+            return Promise.resolve({
+                phoneNumber,
+                success: false,
+                error: 'Invalid phone number'
+              });
+        }
+
+        return client.messages.create({
+            body:msg ,
+            from:twilioNumber,
+            to: "+1" + phone
+        })
+        .then(message => {
+            return {
+                phone,
+                success: true,
+                sid: message.sid
+            }
+        })
+        .catch(error => {
+            return {
+                phone,
+                succes: false,
+                error: error.message
+            }
+        })
+    })
+
+        const msgResults = await Promise.all(messages);
+
+        res.json({
+            total: data.length,
+            results: msgResults
+        })
+    
+        
+
     }
-    catch (err){ 
+    catch (err) {
         console.log(err);
-        res.status(500).send('Error parsing phone numbers data');
+        res.status(500).send('Error processing phone numbers');
+
     }
 
 })
-});
 
 app.listen(port, () => {
   console.log(`mass-texting listening on port ${port}`)
